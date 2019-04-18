3a2a3bd2b197fad21244ceb649872d2a6c7e0dc7
soothsayer136 <bikrum.katwal@gmail.com>
2019-04-18 12:02:19 +0545

    Initial commit

---
+++ app/src/main/java/com/recyclerview/Contact.java
@@ -0,0 +1,44 @@
+package com.recyclerview;
+
+public class Contact {
+    private String name;
+    private String phoneNo;
+    private int imageId;
+
+
+
+    public Contact(String name, String phoneNo, int imageId) {
+        this.name = name;
+        this.phoneNo = phoneNo;
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+
+
+    private int imageId;
+}
+
+
